a0021fd8af6f0305cb15c06894275cfd8e5bfc85
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2024-04-29 22:11:17 +0530

    commit

--- first_nextjs_project/app/Product/[slug]/page.tsx
+++ first_nextjs_project/app/Product/[slug]/page.tsx
@@ -1,9 +1,22 @@
-function page() {
-  return (
-    <div>
-      <h1>product details</h1>
-    </div>
-  );
+import { supabase } from "@/Supabase/apiEntry";
+
+export async function generateStaticParams() {
+  const { data: postData, error } = await supabase.from("Products").select();
+
+  return postData?.map((singlePostData) => {
+    slug: singlePostData.id;
+    // console.log("single post data id", singlePostData.id);
+  });
+}
+
+async function page({ params: { slug } }) {
+  console.log(slug);
+  const { data: postData, error } = await supabase
+    .from("Products")
+    .select()
+    .eq("id", slug);
+  console.log(postData);
+  return <div className="h-[80vh]"></div>;
 }
 
 export default page;
