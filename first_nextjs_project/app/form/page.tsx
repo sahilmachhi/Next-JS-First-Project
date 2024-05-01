c00a5efd6e0ddb862705cf4c12c285cb55988df0
Sahil <113676978+whonix2904@users.noreply.github.com>
2024-05-01 14:41:42 +0530

    commit

--- first_nextjs_project/app/form/page.tsx
+++ first_nextjs_project/app/form/page.tsx
@@ -12,80 +12,80 @@ function page() {
   return (
     <>
       <form action={formAction}>
-        <div class="space-y-12">
-          <div class="border-b border-gray-900/10 pb-12">
-            <h2 class="text-base font-semibold leading-7 text-gray-900">
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
               Personal Information
             </h2>
-            <p class="mt-1 text-sm leading-6 text-gray-600">
+            <p className="mt-1 text-sm leading-6 text-gray-600">
               Use a permanent address where you can receive mail.
             </p>
 
-            <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div class="sm:col-span-3">
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
                 <label
-                  for="productName"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="productName"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Product Name
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     type="text"
                     name="productName"
                     id="productName"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
-              <div class="sm:col-span-3">
+              <div className="sm:col-span-3">
                 <label
-                  for="productDes"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="productDes"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Product Description
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     type="text"
                     name="productDes"
                     id="productDes"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
-              <div class="sm:col-span-4">
+              <div className="sm:col-span-4">
                 <label
-                  for="productPrice"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="productPrice"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   Product Price
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     id="productPrice"
                     name="number"
                     type="number"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
-              <div class="sm:col-span-3">
-                <div class="mt-2">
-                  <div class="col-span-full">
+              <div className="sm:col-span-3">
+                <div className="mt-2">
+                  <div className="col-span-full">
                     <label
-                      for="productPhoto"
-                      class="block text-sm font-medium leading-6 text-gray-900"
+                      htmlFor="productPhoto"
+                      className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Product Photo
                     </label>
-                    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                      <div class="text-center">
+                    <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                      <div className="text-center">
                         <svg
-                          class="mx-auto h-12 w-12 text-gray-300"
+                          className="mx-auto h-12 w-12 text-gray-300"
                           viewBox="0 0 24 24"
                           fill="currentColor"
                           aria-hidden="true"
@@ -96,22 +96,22 @@ function page() {
                             clip-rule="evenodd"
                           />
                         </svg>
-                        <div class="mt-4 flex text-sm leading-6 text-gray-600">
+                        <div className="mt-4 flex text-sm leading-6 text-gray-600">
                           <label
-                            for="productPhoto"
-                            class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                            htmlFor="productPhoto"
+                            className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                           >
                             <span>Upload a file</span>
                             <input
                               id="productPhoto"
                               name="productPhoto"
                               type="file"
-                              class="sr-only"
+                              className="sr-only"
                             />
                           </label>
-                          <p class="pl-1">or drag and drop</p>
+                          <p className="pl-1">or drag and drop</p>
                         </div>
-                        <p class="text-xs leading-5 text-gray-600">
+                        <p className="text-xs leading-5 text-gray-600">
                           PNG, JPG, GIF up to 10MB
                         </p>
                       </div>
@@ -120,40 +120,43 @@ function page() {
                 </div>
               </div>
 
-              <div class="sm:col-span-2 sm:col-start-1">
+              <div className="sm:col-span-2 sm:col-start-1">
                 <label
-                  for="type"
-                  class="block text-sm font-medium leading-6 text-gray-900"
+                  htmlFor="type"
+                  className="block text-sm font-medium leading-6 text-gray-900"
                 >
                   type
                 </label>
-                <div class="mt-2">
+                <div className="mt-2">
                   <input
                     type="text"
                     name="type"
                     id="type"
-                    class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="border-b border-gray-900/10 pb-12">
-            <div class="mt-10 space-y-10">
+          <div className="border-b border-gray-900/10 pb-12">
+            <div className="mt-10 space-y-10">
               <fieldset>
-                <div class="mt-6 space-y-6">
-                  <div class="relative flex gap-x-3">
-                    <div class="flex h-6 items-center">
+                <div className="mt-6 space-y-6">
+                  <div className="relative flex gap-x-3">
+                    <div className="flex h-6 items-center">
                       <input
                         id="isSpacial"
                         name="comments"
                         type="checkbox"
-                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                       />
                     </div>
-                    <div class="text-sm leading-6">
-                      <label for="isSpacial" class="font-medium text-gray-900">
+                    <div className="text-sm leading-6">
+                      <label
+                        htmlFor="isSpacial"
+                        className="font-medium text-gray-900"
+                      >
                         Is Spacial
                       </label>
                     </div>
@@ -164,16 +167,16 @@ function page() {
           </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
+        <div className="mt-6 flex items-center justify-end gap-x-6">
           <button
             type="button"
-            class="text-sm font-semibold leading-6 text-gray-900"
+            className="text-sm font-semibold leading-6 text-gray-900"
           >
             Cancel
           </button>
           <button
             type="submit"
-            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Save
           </button>
